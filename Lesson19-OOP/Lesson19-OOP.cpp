4adf2ba4d7df3a941aa662535372ba7afaa4f128
dudolad_yaroslav <dudolad2009@gmail.com>
2025-07-12 17:43:31 +0300

    Add project files.

---
+++ Lesson19-OOP/Lesson19-OOP.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Student.h"
+using namespace std;
+
+int main()
+{
+    //Student Kyrylo; //name, mark
+    //Kyrylo.setMark(-7865);
+    //Kyrylo.setMark(10);
+    //Student Andriy("Andriy", 12); 
+    //Kyrylo.showInfo();
+    //Andriy.showInfo();
+    //return 0;
+
+
+}
+
---
+++ Lesson19-OOP/Student.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Student
+{
+	char name[100];
+	int mark;
+
+
+public:
+	Student(); //Конструктор за замовчуванням
+	Student(const char* name, int mark); //Конструктор з параметрами
+	~Student(); //Деструктор
+
+
+	void showInfo(); //Прототип
+	void setMark(int user_mark);
+	int getMark();
+
+};
+
