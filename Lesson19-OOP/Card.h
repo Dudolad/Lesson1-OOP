73cca4637e11cdc132c0d70c214a38ec34e2ae4a
dudolad_yaroslav <dudolad2009@gmail.com>
2025-07-12 17:57:10 +0300

    Add class and methods

--- Lesson19-OOP/Card.h
+++ Lesson19-OOP/Card.h
@@ -7,5 +7,15 @@ using namespace std;
 
 class Card
 {
+	int number;
+	char* name;
+	char expiredDate[6];
+	int PIN;
+	double cash;
+public:
+	Card();
+	Card(const char* user_name);
+	~Card();
+	void showInfo();
 };
 
